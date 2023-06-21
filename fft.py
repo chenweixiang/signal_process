c966a92f76bd5ca408e7643f359dc741aab417af
Weixiang Chen <weixiang.chen@outlook.com>
2023-06-22 00:37:32 +0800

    Update fft.py

--- fft.py
+++ fft.py
@@ -5,13 +5,13 @@ from scipy.fftpack import fft
 
 fs = 100 # 采样率
 N = 256 # 数据点数
-n = np.linspace(0,N-1,N)
+n = np.linspace(0, N-1, N)
 print(n)
 
 t = n / fs #时间序列
 print(t)
 
-x = 0.5 * np.sin(2*np.pi*15*t) + 2*np.sin(2*np.pi*40*t) #实信号
+x = 0.5 * np.sin(2 * np.pi * 15 * t) + 2 * np.sin(2 * np.pi * 40 * t) #实信号
 
 y1 = sci.fft.fft(x, N) #信号傅立叶变换
 y2 = sci.fft.fftshift(y1)
@@ -22,24 +22,30 @@ mag2 = abs(y2)
 f1 = n * fs / N #频率序列
 f2 = n * fs / N - fs/2
 
-plt.subplot(3,1,1)
-plt.title("usual FFT")
-plt.xlabel("freq/Hz")
+nrows = 3
+ncols = 1
+index = 0
+
+index += 1
+plt.subplot(nrows,ncols,index)
+plt.title("Usual FFT")
+plt.xlabel("Freq/Hz")
 plt.ylabel("Amp")
 plt.plot(f1, mag1) #随频率变化的振幅
 
-plt.subplot(3,1,2)
+index += 1
+plt.subplot(nrows,ncols,index)
 plt.title("FFT without fftshift")
 plt.xlabel("Freq/Hz")
 plt.ylabel("Amp")
 plt.plot(f2, mag1) #随频率变化的振幅
 
-plt.subplot(3,1,3)
+index += 1
+plt.subplot(nrows,ncols,index)
 plt.title("FFT after fftshift")
 plt.xlabel("Freq/Hz")
 plt.ylabel("Amp")
 plt.plot(f2, mag2) #随频率变化的振幅
 
 plt.tight_layout() #自动调整子图间距
-
 plt.show()
