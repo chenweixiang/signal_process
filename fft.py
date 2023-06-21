62bbf2ce457eb160f92535d65ac7e9ebb3225ef8
Weixiang Chen <weixiang.chen@outlook.com>
2023-06-22 00:37:32 +0800

    Add fft.py

---
+++ fft.py
@@ -0,0 +1,45 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy as sci
+from scipy.fftpack import fft
+
+fs = 100 # 采样率
+N = 256 # 数据点数
+n = np.linspace(0,N-1,N)
+print(n)
+
+t = n / fs #时间序列
+print(t)
+
+x = 0.5 * np.sin(2*np.pi*15*t) + 2*np.sin(2*np.pi*40*t) #实信号
+
+y1 = sci.fft.fft(x, N) #信号傅立叶变换
+y2 = sci.fft.fftshift(y1)
+
+mag1 = abs(y1) #对信号取模求振幅
+mag2 = abs(y2)
+
+f1 = n * fs / N #频率序列
+f2 = n * fs / N - fs/2
+
+plt.subplot(3,1,1)
+plt.title("usual FFT")
+plt.xlabel("freq/Hz")
+plt.ylabel("Amp")
+plt.plot(f1, mag1) #随频率变化的振幅
+
+plt.subplot(3,1,2)
+plt.title("FFT without fftshift")
+plt.xlabel("Freq/Hz")
+plt.ylabel("Amp")
+plt.plot(f2, mag1) #随频率变化的振幅
+
+plt.subplot(3,1,3)
+plt.title("FFT after fftshift")
+plt.xlabel("Freq/Hz")
+plt.ylabel("Amp")
+plt.plot(f2, mag2) #随频率变化的振幅
+
+plt.tight_layout() #自动调整子图间距
+
+plt.show()
